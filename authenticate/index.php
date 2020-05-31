90d9e9dd28b78ee469fcc19e20a1983c54a863dd
tobeh <tobiasch@gmx.at>
2020-05-31 21:01:32 +0200

    Added authentification

--- authenticate/index.php
+++ authenticate/index.php
@@ -1,4 +1,23 @@
 <?php 
-    echo "Hi!";
-    if(isset($_POST['ObserveToken'])) echo 'ObserveToken: ' . $_POST['ObserveToken'];
+    // path to guild list
+    $guildDirectory = '/home/pi/Palantir/';
+
+    // Authenticate server by token
+    if(isset($_POST['ObserveToken'])){
+        $authenticatedGuilds = file_get_contents($guildDirectory . "palantiri.json");
+        $palantiri = json_decode($authenticatedGuilds);
+
+        $authentificatedPalantir;
+
+        foreach($palantiri as $palantir){
+            if($palantir->ObserveToken == $_POST['ObserveToken']){
+                $authentificatedPalantir = $palantir;
+            }
+        }
+
+        if(isset($authentificatedPalantir))
+            echo '{"AuthGuildName": "'.$authentificatedPalantir->ServerName.'", "AuthGuildID": "'.$authentificatedPalantir->GuildID.'", "ObserveToken": "'.$authentificatedPalantir->ObserverToken.'"}';
+        else echo '{"AuthGuildName": "none", "AuthGuildID": "none", "ObserveToken": "' . $_POST['ObserveToken'] . '"}';
+    }
+    else echo '{"AuthGuildName": "none", "AuthGuildID": "none", "ObserveToken": "none"}';
 ?>

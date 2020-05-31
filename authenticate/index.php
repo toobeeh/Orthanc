8f15673dac5715a9d3e8401a837400df7640b9ca
tobeh <tobiasch@gmx.at>
2020-05-31 22:49:46 +0200

    Exportrd verify to external script

--- authenticate/index.php
+++ authenticate/index.php
@@ -1,23 +1,7 @@
 <?php 
-    // path to guild list
-    $guildDirectory = '/home/pi/JsonShared/';
+    if(isset($_GET['ObserveToken'])) $ token = $_GET['ObserveToken'];
 
-    // Authenticate server by token
-    if(isset($_POST['ObserveToken'])){
-        $authenticatedGuilds = file_get_contents($guildDirectory . "palantiri.json");
-        $palantiri = json_decode($authenticatedGuilds);
+    include verify.php;
 
-        $authentificatedPalantir;
-
-        foreach($palantiri as $palantir){
-            if($palantir->ObserveToken == $_POST['ObserveToken']){
-                $authentificatedPalantir = $palantir;
-            }
-        }
-
-        if(isset($authentificatedPalantir))
-            echo '{"Valid": true, "AuthGuildName": "'.$authentificatedPalantir->ServerName.'", "AuthGuildID": '.$authentificatedPalantir->GuildID.', "ObserveToken": "'.$authentificatedPalantir->ObserveToken.'"}';
-        else echo '{"Valid": false, "AuthGuildName": "none", "AuthGuildID": 0, "ObserveToken": "' . $_POST['ObserveToken'] . '"}';
-    }
-    else echo '{"Valid": false, "AuthGuildName": "none", "AuthGuildID": 0, "ObserveToken": "none"}';
+    echo $jsonOutput;   
 ?>
---
+++ authenticate/verify.php
@@ -0,0 +1,28 @@
+<?php
+
+    // processes a $token number including the ObserveToken
+    // outputs a $jsonOutput string including guild information
+
+    // path to guild list
+    $guildDirectory = '/home/pi/JsonShared/';
+
+    // Authenticate server by token
+    if(isset($token)){
+        $authenticatedGuilds = file_get_contents($guildDirectory . "palantiri.json");
+        $palantiri = json_decode($authenticatedGuilds);
+
+        $authentificatedPalantir;
+
+        foreach($palantiri as $palantir){
+            if($palantir->ObserveToken == $token){
+                $authentificatedPalantir = $palantir;
+            }
+        }
+
+        if(isset($authentificatedPalantir))
+            $jsonOutput = '{"Valid": true, "AuthGuildName": "'.$authentificatedPalantir->ServerName.'", "AuthGuildID": '.$authentificatedPalantir->GuildID.', "ObserveToken": "'.$authentificatedPalantir->ObserveToken.'"}';
+        else $jsonOutput = '{"Valid": false, "AuthGuildName": "none", "AuthGuildID": 0, "ObserveToken": "'.$token.'"}';
+    }
+    else $jsonOutput = '{"Valid": false, "AuthGuildName": "none", "AuthGuildID": 0, "ObserveToken": "none"}';
+
+>

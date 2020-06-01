a1c1c736ed5cdf2fe3c71cc016c5a1dfd2fedcc4
tobeh <tobiasch@gmx.at>
2020-06-02 01:40:43 +0200

    Fix depr. last call

--- status/status.php
+++ status/status.php
@@ -17,7 +17,7 @@ if(!$authenticatedGuild->Valid) {
 
 $files = scandir($path);
 foreach($files as $file){
-    if(last($file.explode('/')) == "statusGuild".$authenticatedGuild.'.json') $lobbies = file_get_contents($file);
+    if(end($file.explode('/')) == "statusGuild".$authenticatedGuild.'.json') $lobbies = file_get_contents($file);
 }
 
 $lobbies = '{"Status": "Successful status request", "Lobbies":'.$lobbies.', "Verify":'.$jsonOutput.'}';

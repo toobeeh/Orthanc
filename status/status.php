6d08cfe06cdc6195b05ea66891a01f3ddf7fabb9
tobeh <tobiasch@gmx.at>
2020-06-02 02:04:19 +0200

    object fix

--- status/status.php
+++ status/status.php
@@ -18,9 +18,7 @@ if(!$authenticatedGuild->Valid) {
 $files = array_diff( scandir($path,1), array(".", "..") );
 foreach($files as $file){
     $name = basename($file);
-    vardump($name);
-    echo $name;
-    if($name == "statusGuild".$authenticatedGuild.'.json') $lobbies = file_get_contents($file);
+    if($name == "statusGuild".$authenticatedGuild->AuthGuildName.'.json') $lobbies = file_get_contents($file);
 }
 
 $lobbies = '{"Status": "Successful status request", "Lobbies":'.$lobbies.', "Verify":'.$jsonOutput.'}';

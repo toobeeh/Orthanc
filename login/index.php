de41678aca12508c4f11acd06ce994f1a9cabe39
tobeh <tobiasch@gmx.at>
2020-06-04 22:25:00 +0200

    login returns stuff

--- login/index.php
+++ login/index.php
@@ -3,8 +3,10 @@
     ini_set('display_startup_errors', 1); 
     error_reporting(E_ALL);
 
-    if(isset($_POST['discordTag'])) $tag = $_POST['discordTag'];
+    if(isset($_POST['login'])) $login = $_POST['login'];
 
     include 'login.php';
+
+    echo $valid;
 
 ?>
--- login/login.php
+++ login/login.php
@@ -1,9 +1,23 @@
 <?php 
+
+    $membersPath = "/home/pi/JsonShared/members.json";
+
     ini_set('display_errors', 1); 
     ini_set('display_startup_errors', 1); 
     error_reporting(E_ALL);
 
-    if(!isset($tag))return;
+    if(!isset($login)){
+        $valid = '{"Valid":false}';
+        return;
+    }
 
-    file_put_contents('/home/pi/JsonShared/Members/' . $tag .'.login', $tag);
+    $validUser;
+
+    $members = json_decode(file_get_contents($membersPath));
+    foreach($members as $member){
+        if($member->UserLogin == $login)  $validUser = $member;
+    }
+
+    if(isset($validUser)) $valid = '{"Valid":true, "User":' . json_encode($validUser) . "}";
+    else  $valid = '{"Valid":false}';
 ?>

f65a1a40319b6042b74a5f6dfc095bd6f9c82d8d
tobeh <tobiasch@gmx.at>
2020-06-05 17:41:23 +0200

    fix typos

--- memberstate/memberstate.php
+++ memberstate/memberstate.php
@@ -4,14 +4,14 @@
     // processes a $playerstatus json string
 
     if(!isset($playerstatus)) return;
-    $status = json_encode($playerstatus);
+    $status = json_decode($playerstatus);
 
     $guildDirectory = '/home/pi/JsonShared/';
     $members = json_decode(file_get_contents($guildDirectory . 'members.json'));
 
     $authMember;
     foreach($members as $savedMember){
-        if ($savedMember->UserID == $status->Playermember->UserID && $savedMember->UserLogin == $status->Playermember->UserLogin)
+        if ($savedMember->UserID == $status->PlayerMember->UserID && $savedMember->UserLogin == $status->PlayerMember->UserLogin)
             $authMember = $savedMember;
     }
 

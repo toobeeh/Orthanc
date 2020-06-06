601864ad4945e587802b3c7c8de3be6e52859b17
tobeh <tobiasch@gmx.at>
2020-06-06 22:57:07 +0200

    added provider

---
+++ idprovider/provider.php
@@ -0,0 +1,55 @@
+<?php 
+    ini_set('display_errors', 1); 
+    ini_set('display_startup_errors', 1); 
+    error_reporting(E_ALL);
+    $guildDirectory = '/home/pi/JsonShared/';
+
+    // processes a lobby $key, if not present generate a $id 
+
+    if(!isset($member) || !isset($key)) {
+        $result = '{"Valid": false, "Member":null}';
+        return;
+    }
+
+    // verify member
+    $members = json_decode(file_get_contents($guildDirectory . 'members.json'));
+    $sender = json_decode($member);
+
+    $authenticatedMember;
+    foreach($members as $savedMember){
+        if ($savedMember->UserID == $sender->UserID && $savedMember->UserLogin == $sender->UserLogin) $authenticatedMember = $savedMember;
+    }
+
+    if(!isset($authenticatedMember)) {
+        $result = '{"Valid": false, "Member":' . json_decode($member) . '}';
+        return;
+    }
+
+    // if id is not set, search for lobbies with same key
+    if(!isset($id)){
+        $lobbies = json_encode($guildDirectory . "lobbies.json");
+
+        foreach($lobbies as $lobby){
+            if ($lobby->Key == $key) $id=$lobby->ID;
+        }
+
+        // if no lobby with same key is present, generate new id
+        if(!isset($id)) $id =  str_pad(mt_rand(1,99999999),8,'0',STR_PAD_LEFT);
+        $jsonLobby = json_decode('{"ID":"' . $id . ', "Key":"' . $key . '"}');
+        array_push($lobbies, $jsonLobby);
+        file_put_contents("lobbies.json", json_decode($lobbies));
+        rename("lobbies.json", $guildDirectory . "lobbies.json");
+        $result = '{"Valid": true, "Member":' . json_decode($member) . ', "Lobby":' . json_decode($jsonLobby) . '}';
+    }
+    else{
+        $lobbies = json_encode($guildDirectory . "lobbies.json");
+
+        $match;
+        foreach($lobbies as $lobby){
+            if ($lobby->ID == $id) {$lobby->Key = $key; $match = $lobby;}
+        }
+        file_put_contents("lobbies.json", json_decode($lobbies));
+        rename("lobbies.json", $guildDirectory . "lobbies.json");
+        $result = '{"Valid": true, "Member":' . json_decode($member) . ', "Lobby":' . json_decode($match) . '}';
+    }
+?>

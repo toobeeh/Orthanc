b12c0c2eef4e6d0dd6cb613c315c9e31c436228c
tobeh <tobiasch@gmx.at>
2020-06-07 17:41:34 +0200

    re debug

--- login/login.php
+++ login/login.php
@@ -2,9 +2,6 @@
 
     $membersPath = "/home/pi/JsonShared/members.json";
 
-    // ini_set('display_errors', 1); 
-    // ini_set('display_startup_errors', 1); 
-    // error_reporting(E_ALL);
 
     if(!isset($login)){
         $valid = '{"Valid":false}';

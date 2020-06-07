746103e3b3f204b9e73b191bd1cbba09a47db57b
tobeh <tobiasch@gmx.at>
2020-06-07 17:27:13 +0200

    Remove all debug

--- login/login.php
+++ login/login.php
@@ -2,9 +2,9 @@
 
     $membersPath = "/home/pi/JsonShared/members.json";
 
-    ini_set('display_errors', 1); 
-    ini_set('display_startup_errors', 1); 
-    error_reporting(E_ALL);
+    // ini_set('display_errors', 1); 
+    // ini_set('display_startup_errors', 1); 
+    // error_reporting(E_ALL);
 
     if(!isset($login)){
         $valid = '{"Valid":false}';
--- verify/index.php
+++ verify/index.php
@@ -1,7 +1,7 @@
 <?php 
-    ini_set('display_errors', 1); 
-    ini_set('display_startup_errors', 1); 
-    error_reporting(E_ALL);
+    // ini_set('display_errors', 1); 
+    // ini_set('display_startup_errors', 1); 
+    // error_reporting(E_ALL);
 
     if(isset($_POST['observeToken'])) $token = $_POST['observeToken'];
     if(isset($_POST['member'])) $member = $_POST['member'];

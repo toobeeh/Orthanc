abcf273a2f56772c49321cb5ef392e46f5c2d18a
tobeh <tobiasch@gmx.at>
2020-06-08 20:45:45 +0200

    fix

--- db.php
+++ db.php
@@ -1,11 +1,12 @@
 <?php 
 
-$_db = new SQlite3('/home/pi/Database/palantir.db');
+
 
 function getMemberJSON($_login){
+    $_db = new SQlite3('/home/pi/Database/palantir.db');
     $_sql = $_db->prepare('SELECT * FROM Members WHERE Login = ?');
-    $_sql->bindParam(1, $login);
-    $_result = $sql->execute();
+    $_sql->bindParam(1, $_login);
+    $_result = $_sql->execute();
     if($_row = $_result->fetchArray()) return $_row['Member'];
     else return false;
 }

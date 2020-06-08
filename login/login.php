31ec300be34fcd150d5f39a5d060ec462cb00017
tobeh <tobiasch@gmx.at>
2020-06-08 02:04:22 +0200

    debug

--- login/login.php
+++ login/login.php
@@ -9,12 +9,13 @@
     }
 
     $validUser;
+    $cont = file_get_contents($membersPath);
 
-    $members = json_decode(file_get_contents($membersPath));
+    $members = json_decode($cont);
     foreach($members as $member){
         if($member->UserLogin == $login)  $validUser = $member;
     }
 
     if(isset($validUser)) $valid = '{"Valid":true, "Member":' . json_encode($validUser) . "}";
-    else  $valid = '{"Valid":false}';
+    else  $valid = '{"Valid":false, "Error": "' . json_last_error_msg() . '", "Content": "' . $cont . '"}';
 ?>

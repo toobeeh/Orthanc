a0597f2629a336881b5eb746a36d69a1d7d487d9
tobeh <tobiasch@gmx.at>
2020-06-10 00:41:29 +0200

    improve db settings

--- db.php
+++ db.php
@@ -9,6 +9,9 @@
 // Check if members has row with login
 function getMemberJSON($_login){
     $_db = new SQlite3('/home/pi/Database/palantir.db');
+    $_db->busyTimeout(5000);
+    $_db->exec('PRAGMA journal_mode = wal;');
+
     $_sql = $_db->prepare('SELECT * FROM Members WHERE Login = ?');
     $_sql->bindParam(1, $_login);
     $_result = $_sql->execute();
@@ -21,6 +24,9 @@ function getMemberJSON($_login){
 // Set member Json (for example to add new guild)
 function setMemberJSON($_login, $_json){
     $_db = new SQlite3('/home/pi/Database/palantir.db');
+    $_db->busyTimeout(5000);
+    $_db->exec('PRAGMA journal_mode = wal;');
+
     $_sql = $_db->prepare('UPDATE Members SET Member = ? WHERE Login = ?');
     $_sql->bindParam(1, $_json);
     $_sql->bindParam(2, $_login);
@@ -37,6 +43,9 @@ function setMemberJSON($_login, $_json){
 // Table is read-only, as only palantir bot collects lobby data and generates a guild lobby array
 function getGuildLobbiesJSON($_guildID){
     $_db = new SQlite3('/home/pi/Database/palantir.db');
+    $_db->busyTimeout(5000);
+    $_db->exec('PRAGMA journal_mode = wal;');
+
     $_sql = $_db->prepare('SELECT * FROM GuildLobbies WHERE GuildID = ?');
     $_sql->bindParam(1, $_guildID);
     $_result = $_sql->execute();
@@ -54,6 +63,9 @@ function getGuildLobbiesJSON($_guildID){
 // Table is read-only, as only palantir bot listens to discord commands to modify settings
 function getPalantirJSON($_observeToken){
     $_db = new SQlite3('/home/pi/Database/palantir.db');
+    $_db->busyTimeout(5000);
+    $_db->exec('PRAGMA journal_mode = wal;');
+
     $_sql = $_db->prepare('SELECT * FROM Palantiri WHERE Token = ?');
     $_sql->bindParam(1, $_observeToken);
     $_result = $_sql->execute();
@@ -70,6 +82,9 @@ function getPalantirJSON($_observeToken){
 // Find a lobby by a lobby key
 function getLobbyJSONByKey($_lobbyKey){
     $_db = new SQlite3('/home/pi/Database/palantir.db');
+    $_db->busyTimeout(5000);
+    $_db->exec('PRAGMA journal_mode = wal;');
+
     $_sql = $_db->prepare('SELECT * FROM Lobbies WHERE Lobby LIKE ?');
     $_lobbyKey = "%" . $_lobbyKey . "%";
     $_sql->bindParam(1, $_lobbyKey);
@@ -83,6 +98,9 @@ function getLobbyJSONByKey($_lobbyKey){
 // Find a lobby by a lobby id
 function getLobbyJSONByID($_lobbyID){
     $_db = new SQlite3('/home/pi/Database/palantir.db');
+    $_db->busyTimeout(5000);
+    $_db->exec('PRAGMA journal_mode = wal;');
+
     $_sql = $_db->prepare('SELECT * FROM Lobbies WHERE LobbyID = ?');
     $_sql->bindParam(1, $_lobbyID);
     $_result = $_sql->execute();
@@ -95,6 +113,9 @@ function getLobbyJSONByID($_lobbyID){
 // update the lobby data for a lobby id
 function updateLobbyJSON($_lobbyID, $_lobbyJson){
     $_db = new SQlite3('/home/pi/Database/palantir.db');
+    $_db->busyTimeout(5000);
+    $_db->exec('PRAGMA journal_mode = wal;');
+
     $_sql = $_db->prepare('UPDATE Lobbies SET Lobby = ? WHERE LobbyID = ?');
     $_sql->bindParam(1, $_lobbyJson);
     $_sql->bindParam(2, $_lobbyID);
@@ -106,6 +127,9 @@ function updateLobbyJSON($_lobbyID, $_lobbyJson){
 // add a new lobby
 function addLobby($_lobbyID, $_lobbyJson){
     $_db = new SQlite3('/home/pi/Database/palantir.db');
+    $_db->busyTimeout(5000);
+    $_db->exec('PRAGMA journal_mode = wal;');
+
     $_sql = $_db->prepare('INSERT INTO Lobbies VALUES (?, ?)');
     $_sql->bindParam(1, $_lobbyID);
     $_sql->bindParam(2, $_lobbyJson);
@@ -120,6 +144,9 @@ function addLobby($_lobbyID, $_lobbyJson){
 
 function writeReport($_lobbyID, $_observeToken, $_reportJson){
     $_db = new SQlite3('/home/pi/Database/palantir.db');
+    $_db->busyTimeout(5000);
+    $_db->exec('PRAGMA journal_mode = wal;');
+
     $_sql = $_db->prepare("REPLACE INTO Reports VALUES(?, ?, ?, datetime('now'))");
     $_sql->bindParam(1, $_lobbyID);
     $_sql->bindParam(2, $_observeToken);
@@ -138,6 +165,9 @@ function writeReport($_lobbyID, $_observeToken, $_reportJson){
 
 function writeStatus($_statusJSON){
     $_db = new SQlite3('/home/pi/Database/palantir.db');
+    $_db->busyTimeout(5000);
+    $_db->exec('PRAGMA journal_mode = wal;');
+
     $_sql = $_db->prepare("INSERT INTO Status VALUES(?, datetime('now'))");
     $_sql->bindParam(1, $_statusJSON);
     $_result = $_sql->execute();

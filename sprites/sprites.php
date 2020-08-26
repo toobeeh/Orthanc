ae59532f6fa89e11f3d8986feefc8b145b0d6512
tobeh <tobiasch@gmx.at>
2020-08-26 11:56:14 +0200

    fix json

--- sprites/sprites.php
+++ sprites/sprites.php
@@ -1,5 +1,5 @@
 <?php
     // gets all online sprite maps
     include '/home/pi/Webroot/Orthanc/db.php';
-    $result = "{'OnlineSprites' : " . getSprites() . ", 'Sprites': " . getAvailableSprites() . "}";
+    $result = '{"OnlineSprites" : ' . getSprites() . ', "Sprites": ' . getAvailableSprites() . "}";
 ?>

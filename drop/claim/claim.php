50c47bb2416d38381ff823b89486d568682e5b2e
tobeh <tobiasch@gmx.at>
2020-08-30 23:58:54 +0200

    Add checks

--- drop/claim/claim.php
+++ drop/claim/claim.php
@@ -1,6 +1,6 @@
 <?php 
     include '/home/pi/Webroot/Orthanc/db.php';
-    if(getMemberJSON($login) != false){
+    if(getMemberJSON($login) != false && isset($_dropID) && isset($_lobbyKey) && isset($_lobbyPlayerID)){
         $result = claimDrop($dropID,$lobbyKey,$lobbyPlayerID);
     }
     else $result = '{"Valid":false, "Login: "' . $login . '"}';
--- drop/claim/index.php
+++ drop/claim/index.php
@@ -1,5 +1,9 @@
 <?php
-
+    
+    ini_set('display_errors', 1); 
+    ini_set('display_startup_errors', 1); 
+    error_reporting(E_ALL);
+    
     // checks if a drop is available atm
     if(isset($_POST['login'])) $login = $_POST['login'];
     if(isset($_POST['dropID'])) $dropID = $_POST['dropID'];

35f80cc3e045b83cdebaa6a132d963431d511fcf
tobeh <tobiasch@gmx.at>
2020-08-31 00:40:32 +0200

    err on

--- drop/claim/claim.php
+++ drop/claim/claim.php
@@ -1,4 +1,8 @@
 <?php 
+
+    ini_set('display_errors', 1); 
+    ini_set('display_startup_errors', 1); 
+    error_reporting(E_ALL); 
     include '/home/pi/Webroot/Orthanc/db.php';
     if(getMemberJSON($login) != false && isset($dropID) && isset($lobbyKey) && isset($lobbyPlayerID)){
         $result = claimDrop($dropID,$lobbyKey,$lobbyPlayerID);

09a3445208e98f5fcfd8d76ad2b700002586bd3c
tobeh <tobiasch@gmx.at>
2020-09-01 13:37:40 +0200

    add gif title

--- sprites/gif/index.php
+++ sprites/gif/index.php
@@ -4,16 +4,26 @@
     <meta charset="utf-8">
     <title>Sprites</title>
     <link rel="stylesheet" type="text/css" href="/Orthanc/popup.css">
+    <style>
+        .flexcenter{
+                    justify-content: center;
+                }
+        body{width:100%}
+
+    </style>
 </head>
 <body>
 
-    <h1>Listig all available Sprites</h1>
+    <h1>Listing all available Sprites</h1>
     <div class = 'flexcenter flexrow'>
 <?php
+function endsWith($haystack, $needle) {
+    return substr_compare($haystack, $needle, -strlen($needle)) === 0;
+}
 
 $gifs = array_diff(scandir("."), array('..', '.'));
 foreach($gifs as $gif){
-    if($gif != "drop.gif") echo "<div><img src='".$gif."'></div>";
+    if($gif != "drop.gif" && endsWith($gif,".gif")) echo "<div><img src='".$gif."'><h2>" . str_replace(".gif","",$gif) . "</h2></div>";
 }
 
 ?>

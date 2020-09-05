835a6ab3983b755baaf4be4d3d849000deb3a991
tobeh <tobiasch@gmx.at>
2020-09-05 15:34:06 +0200

    add update button

--- log/index.php
+++ log/index.php
@@ -1,3 +1,5 @@
+<head><title>PTR Log</title></head>
+<body>
 <?php
 
     ini_set('display_errors', 1); 
@@ -8,12 +10,22 @@
         die("<h2>401: Unauthorized</h2>");
     }
     
-    echo "<h2>Plantir Bot Log</h2><h3> Refreshed at:  " . date("Y-m-d H:i:s") . "</h3><br>";
-    $file = file("/home/pi/palantirOutput.log");
-    $file = array_reverse($file);
-    foreach($file as $f){
-        echo $f."<br />";
+    if(!isset($_GET['update'])){
+        echo "<h2>Plantir Bot Log</h2><h3> Refreshed at:  " . date("Y-m-d H:i:s") . "</h3><button onlick='location.href = \"Orthanc/log/?auth=supersecret?update\"'>Pull & Restart PTR</button><br>";
+        $file = file("/home/pi/palantirOutput.log");
+        $file = array_reverse($file);
+        foreach($file as $f){
+            echo $f."<br />";
+        }
+        echo "<script>setInterval(()=>{if(window.scrollY == 0)location.reload();},2000);</script>";
     }
-    echo "<script>setInterval(()=>{if(window.scrollY == 0)location.reload();},2000);</script>";
-
+    else{
+        echo "> " . shell_exec("sudo service palantir stop") . "<br>"
+        echo "> " . shell_exec(">/home/pi/palantirOutput.log") . "<br>"
+        echo "> " . shell_exec("sudo git -C /home/pi/Palantir/ pull") . "<br>"
+        echo "> " . shell_exec("sudo service palantir start") . "<br>"
+        echo "<button onlick='location.href = \"Orthanc/log/?auth=supersecret\"'>Show log</button>"
+    }
+    
 ?>
+</body>

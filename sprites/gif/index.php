b2157511fbc802678fd1291f62f18a5e6ec50a8a
tobeh <tobiasch@gmx.at>
2020-09-07 00:14:31 +0200

    add chicks gif and order button

--- sprites/gif/index.php
+++ sprites/gif/index.php
@@ -1,3 +1,9 @@
+<?php 
+ini_set('display_errors', 1); 
+ini_set('display_startup_errors', 1); 
+error_reporting(E_ALL);
+include '/home/pi/Webroot/Orthanc/db.php';
+?>
 <!doctype html>
 <html>
 <head>
@@ -26,14 +32,11 @@
 <body>
 
     <h1>Listing all available Sprites</h1>
+    <a href= <?php echo isset($_GET["price"]) ? "/Orthanc/sprites/gif/" : "/Orthanc/sprites/gif/?price"; ?> >
+    <h2>Order by <?php echo isset($_GET["price"]) ? " ID" : " price"; ?> </h2>
+    </a>
     <div class = 'flexcenter flexrow'>
 <?php
-
-
-ini_set('display_errors', 1); 
-ini_set('display_startup_errors', 1); 
-error_reporting(E_ALL);
-include '/home/pi/Webroot/Orthanc/db.php';
 
 function endsWith($haystack, $needle) {
     return substr_compare($haystack, $needle, -strlen($needle)) === 0;
@@ -43,7 +46,8 @@ $gifs = array_diff(scandir("."), array('..', '.'));
 foreach($gifs as $gif){
     if($gif != "drop.gif" && endsWith($gif,".gif")) {
         $sprite = getSpriteByGifName($gif);
-        echo "<div class='sprite flexcol flexcenter' style='order:" . $sprite['Cost'] ."'><h2>"."#". $sprite['ID'] 
+        echo "<div class='sprite flexcol flexcenter' style='order:" 
+            . (isset($_GET["price"]) ? $sprite['Cost'] : $sprite['ID']) ."'><h2>"."#". $sprite['ID'] 
             ."</h2><img style='width:100%' src='".$gif."'><h2>" 
             . $sprite['Name'] . "</h2><h3>Costs: "
             . $sprite['Cost'] . " Bubbles</h2>" . "</div>";

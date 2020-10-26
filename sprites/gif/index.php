5fbf212503e6033533c42050c4a60d8fb5aebf29
tobeh <tobiasch@gmx.at>
2020-10-26 17:54:21 +0100

    change href wrap

--- sprites/gif/index.php
+++ sprites/gif/index.php
@@ -49,11 +49,11 @@ $gifs = array_diff(scandir("."), array('..', '.'));
 foreach($gifs as $gif){
     if($gif != "drop.gif" && endsWith($gif,".gif")) {
         $sprite = getSpriteByGifName($gif);
-        echo "<div class='sprite flexcol flexcenter' style='order:" 
+        echo "<a href='/Orthanc/sprites/cabin/?sprite=" . $sprite['ID'] . "'><div class='sprite flexcol flexcenter' style='order:" 
             . (isset($_GET["price"]) ? $sprite['Cost'] : $sprite['ID']) ."'><h2>"."#". $sprite['ID'] 
-            ."</h2><a href='/Orthanc/sprites/cabin/?sprite=" . $sprite['ID'] . "'> <img style='width:100%' src='".$gif."'></a><h2>" 
+            ."</h2> <img style='width:100%' src='".$gif."'><h2>" 
             . $sprite['Name'] . "</h2><h3>Costs: "
-            . $sprite['Cost'] . " Bubbles</h2>" . "</div>";
+            . $sprite['Cost'] . " Bubbles</h2>" . "</div></a>";
     }
 }
 

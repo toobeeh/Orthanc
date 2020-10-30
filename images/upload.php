6f5ad0aa9ea58686555cf67f1609d60fed874097
tobeh <tobiasch@gmx.at>
2020-10-30 21:18:58 +0100

    img upload

---
+++ images/upload.php
@@ -0,0 +1,18 @@
+<?php 
+$image = $_POST["image"];
+
+
+    $ifp = fopen( "img.png", 'wb' ); 
+
+    // split the string on commas
+    // $data[ 0 ] == "data:image/png;base64"
+    // $data[ 1 ] == <actual base64 string>
+    $data = explode( ',', $image );
+
+    // we could add validation here with ensuring count( $data ) > 1
+    fwrite( $ifp, base64_decode( $data[ 1 ] ) );
+
+    // clean up the file resource
+    fclose( $ifp ); 
+
+    header("Location: img.png");

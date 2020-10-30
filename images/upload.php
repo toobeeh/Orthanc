f0748fd1f261d0c0c179deb1f35f5d10e04520e7
tobeh <tobiasch@gmx.at>
2020-10-30 21:48:49 +0100

    fix typo

--- images/upload.php
+++ images/upload.php
@@ -4,13 +4,13 @@ $name = $_POST["name"];
 $rnd = substr(str_shuffle(str_repeat($x='0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ', ceil(10/strlen($x)) )),1,10);
 $name = $name . $rnd;
 
-echo $image;
+echo $name;
 $ifp = fopen( $name . ".png", 'wb' ); 
 
 // split the string on commas
 // $data[ 0 ] == "data:image/png;base64"
 // $data[ 1 ] == <actual base64 string>
-$data = explode( ',', $name );
+$data = explode( ',', $image );
 
 // we could add validation here with ensuring count( $data ) > 1
 fwrite( $ifp, base64_decode( $data[ 1 ] ) );

85b616508a6fcb7d7487482df0ece8b555ec76bb
tobeh <tobiasch@gmx.at>
2020-10-30 21:35:06 +0100

    fileupload

--- images/upload.php
+++ images/upload.php
@@ -1,18 +1,18 @@
 <?php 
 $image = $_POST["image"];
+$name = $_POST["name"];
+$name = $name . microtime();
 
-    echo $image;
-    $ifp = fopen( "img.png", 'wb' ); 
+echo $name;
+$ifp = fopen( "img.png", 'wb' ); 
 
-    // split the string on commas
-    // $data[ 0 ] == "data:image/png;base64"
-    // $data[ 1 ] == <actual base64 string>
-    $data = explode( ',', $image );
+// split the string on commas
+// $data[ 0 ] == "data:image/png;base64"
+// $data[ 1 ] == <actual base64 string>
+$data = explode( ',', $name );
 
-    // we could add validation here with ensuring count( $data ) > 1
-    fwrite( $ifp, base64_decode( $data[ 1 ] ) );
+// we could add validation here with ensuring count( $data ) > 1
+fwrite( $ifp, base64_decode( $data[ 1 ] ) );
 
-    // clean up the file resource
-    fclose( $ifp ); 
-
-    //header("Location: img.png");
+// clean up the file resource
+fclose( $ifp ); 

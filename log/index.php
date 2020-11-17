377ca407bcd88b070129d191cbe6b03d625d912d
tobeh <tobiasch@gmx.at>
2020-11-17 21:49:21 +0100

    fix err

--- log/index.php
+++ log/index.php
@@ -27,11 +27,11 @@
         echo "<h2>Plantir Bot Dashboard & Log</h2><h3> Refreshed at:  " . date("Y-m-d H:i:s") 
             . "</h3><a href='/Orthanc/log/?auth=supersecret&updateptr'><button>Pull & Restart Palantir</button></a><br><a href='/Orthanc/log/?auth=supersecret&updateohc'><button>Pull Orthanc</button></a><br><br>";
         $file = file("/home/pi/palantirOutput.log");
-        if(count($file) > 500) file_put_contents("/home/pi/palantirOutput.log"), $file[0];
         $file = array_reverse($file);
         foreach($file as $f){
             echo $f."<br />";
         }
+        if(count($file) > 500) shell_exec("sudo truncate -s0 /home/pi/palantirOutput.log 2>&1");
         echo "<script>setInterval(()=>{if(window.scrollY == 0)location.reload();},2000);</script>";
     }
     

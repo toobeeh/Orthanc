2f99783587fe5c5b0423cf743eab24e02f84eee5
Tobi S <tobiasch@gmx.at>
2021-09-12 11:00:30 +0200

    Add oauth2 try

---
+++ auth/index.php
@@ -0,0 +1,47 @@
+<?php
+
+require __DIR__ . '/vendor/autoload.php';
+
+use Xwilarg\Discord\OAuth2;
+
+// CLIENT-ID-HERE: Replace this with the Client ID of the application
+// SECRET: Replace this with the Secret of the application
+// CALLBACK-URL: Replace this with the redirect URL (URL called after the user is logged in, must be registered in https://discordapp.com/developers/applications/[YourAppId]/oauth)
+// Enter your Discord Oauth details here:
+$oauth2 = new OAuth2("715874397025468417", file_get_contents("/home/pi/oauth2Secret.txt"), "https://tobeh.host/Orthanc/auth/");
+
+if ($oauth2->isRedirected() === false) { // Did the client already logged in ?
+    // The parameters can be a combination of the following: connections, email, identity or guilds
+    // More information about it here: https://discordapp.com/developers/docs/topics/oauth2#shared-resources-oauth2-scopes
+    // The others parameters are not available with this library
+    $oauth2->startRedirection(['identify', 'connections']);
+} else {
+    // We preload the token to see if everything happened without error
+    $ok = $oauth2->loadToken();
+    if ($ok !== true) {
+        // A common error can be to reload the page because the code returned by Discord would still be present in the URL
+        // If this happen, isRedirected will return true and we will come here with an invalid code
+        // So if there is a problem, we redirect the user to Discord authentification
+        $oauth2->startRedirection(['identify', 'connections']);
+    } else {
+        // ---------- USER INFORMATION
+        $answer = $oauth2->getUserInformation(); // Same as $oauth2->getCustomInformation('users/@me')
+        if (array_key_exists("code", $answer)) {
+            exit("An error occured: " . $answer["message"]);
+        } else {
+            echo "Welcome " . $answer["username"] . "#" . $answer["discriminator"];
+        }
+
+        echo '<br/><br/>';
+        // ---------- CONNECTIONS INFORMATION
+        $answer = $oauth2->getConnectionsInformation();
+        if (array_key_exists("code", $answer)) {
+            exit("An error occured: " . $answer["message"]);
+        } else {
+            foreach ($answer as $a) {
+                echo $a["type"] . ': ' . $a["name"] . '<br/>';
+            }
+        }
+    }
+}
+?>

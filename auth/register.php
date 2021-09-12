a2d80783ce0f4a00a2f96da9e416181d7a6409d2
Tobi S <tobiasch@gmx.at>
2021-09-12 12:39:24 +0200

    errores

--- auth/register.php
+++ auth/register.php
@@ -1,4 +1,7 @@
 <?php 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 if(isset($_GET["id"]) && isset($_GET["username"])) {
     include '/home/pi/Webroot/Orthanc/db.php';
 

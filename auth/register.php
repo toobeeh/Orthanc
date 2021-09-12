bec9cb2ecc292d3297a96dccc83609ba931389b6
Tobi S <tobiasch@gmx.at>
2021-09-12 12:36:14 +0200

    not die

--- auth/register.php
+++ auth/register.php
@@ -1,13 +1,17 @@
 <?php 
-if(!isset($_GET["id"]) || !isset($_GET["username"])) die();
-include '/home/pi/Webroot/Orthanc/db.php';
+if(isset($_GET["id"]) && isset($_GET["username"])) {
+    include '/home/pi/Webroot/Orthanc/db.php';
 
-$id = $_GET["id"];
-$username = $_GET["username"];
-$login; // find unique login
-do{
-    $login = mt_rand(0,999999);
+    $id = $_GET["id"];
+    $username = $_GET["username"];
+    $login; // find unique login
+    do{
+        $login = mt_rand(0,999999);
+    }
+    while(getMemberJSON($login));
+    addMember($login, $username, $id);
+    header("Location: https://skribbl.io/?login=" . $login);
 }
-while(getMemberJSON($login));
-addMember($login, $username, $id);
-header("Location: https://skribbl.io/?login=" . $login);
+else {
+    header("Location: https://skribbl.io/");
+}

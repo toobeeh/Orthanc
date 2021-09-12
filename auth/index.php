3f6b312761e5251d8ffc9d52fcb8e53da70629f7
Tobi S <tobiasch@gmx.at>
2021-09-12 11:21:22 +0200

    fix secret

--- auth/index.php
+++ auth/index.php
@@ -16,7 +16,7 @@ if ($oauth2->isRedirected() === false) { // Did the client already logged in ?
     // The parameters can be a combination of the following: connections, email, identity or guilds
     // More information about it here: https://discordapp.com/developers/docs/topics/oauth2#shared-resources-oauth2-scopes
     // The others parameters are not available with this library
-    $oauth2->startRedirection(['identify', 'connections']);
+    $oauth2->startRedirection(['identify']);
 } else {
     // We preload the token to see if everything happened without error
     $ok = $oauth2->loadToken();
@@ -24,7 +24,7 @@ if ($oauth2->isRedirected() === false) { // Did the client already logged in ?
         // A common error can be to reload the page because the code returned by Discord would still be present in the URL
         // If this happen, isRedirected will return true and we will come here with an invalid code
         // So if there is a problem, we redirect the user to Discord authentification
-        $oauth2->startRedirection(['identify', 'connections']);
+        $oauth2->startRedirection(['identify']);
     } else {
         // ---------- USER INFORMATION
         $answer = $oauth2->getUserInformation(); // Same as $oauth2->getCustomInformation('users/@me')

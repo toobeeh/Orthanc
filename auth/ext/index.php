ca66d82cff61354632529a8405933cf511373094
tobeh <tobiasch@gmx.at>
2021-11-11 23:10:22 +0100

    fix dir

--- auth/ext/index.php
+++ auth/ext/index.php
@@ -2,7 +2,7 @@
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-require __DIR__ . '/vendor/autoload.php';
+require dirname(__DIR__, 1) . '/vendor/autoload.php';
 
 use Xwilarg\Discord\OAuth2;
 $secret = trim(file_get_contents("/home/pi/oauth2Secret.txt"));

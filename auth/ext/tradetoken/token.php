5e6bfe8fa6483d39b36dfe868cd082f933949b1a
tobeh <tobiasch@gmx.at>
2021-11-11 22:46:10 +0100

    Add tokentrade endpoint

---
+++ auth/ext/tradetoken/token.php
@@ -0,0 +1,17 @@
+<?php 
+    // login token set?
+    if(!isset($token)){
+        $valid = '{"Valid":false}';
+        return;
+    }
+
+    // include db functions
+    include '/home/pi/Webroot/Orthanc/db.php';
+
+    // get matching member from db
+    $login = getMemberLoginByToken($token);
+
+    // evaluate member
+    if($login !== false) $valid = '{"Valid":true, "Login":' . $token . "}";
+    else $valid = '{"Valid":false, "Token: "' . $token . '"}';
+?>

25a15262695fa4d054a2e4cda26c73b48503ab67
tobeh <tobiasch@gmx.at>
2021-11-18 00:58:54 +0100

    change to strpos

--- serverinvite/index.php
+++ serverinvite/index.php
@@ -3,7 +3,7 @@
 ini_set('display_errors', 1); 
 ini_set('display_startup_errors', 1); 
 error_reporting(E_ALL);
-if(!str_contains($_SERVER['HTTP_USER_AGENT'], "Discordbot")) header("Location: http://typo.rip#u"); 
+if(false == strpos($_SERVER['HTTP_USER_AGENT'], "Discordbot")) header("Location: http://typo.rip#u"); 
 ?>
     <!DOCTYPE html>
 <html>

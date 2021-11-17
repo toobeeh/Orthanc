69d8be4db3c78ad8e580301abbbb92eb91d932cf
tobeh <tobiasch@gmx.at>
2021-11-18 00:56:24 +0100

    add redirect to not-discord-bot

---
+++ serverinvite/index.php
@@ -0,0 +1,16 @@
+<?php if(!str_contains($_SERVER['HTTP_USER_AGENT'], "Discordbot")) header("Location: http://typo.rip#u"); ?>
+    <!DOCTYPE html>
+<html>
+<head>
+<title>Connect Server to Palantir</title>
+<meta charset="UTF-8">
+<!-- Twitter Card data -->
+<meta name="twitter:card" content="summary_large_image">
+<!-- Open Graph data -->
+<meta property="og:title" content=">Connect Server to Palantir" />
+<meta property="og:url" content="https://" />
+<meta property="og:image" content="https://" />
+<meta property="og:site_name" content="typo.rip" />
+<meta name="description" content="Crash course on Python offered by Google on Coursera and curated by The Wind LLC">
+<meta name="author" content="C Brennan Poole">
+</head>

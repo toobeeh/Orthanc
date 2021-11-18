30faa90c236b040fd16726f876d1628812be3826
tobeh <tobiasch@gmx.at>
2021-11-18 19:05:53 +0100

    not restcord

--- serverinvite/guildtest.php
+++ serverinvite/guildtest.php
@@ -1,13 +1,11 @@
-<?php $guild = $_GET["guild"];
-ini_set('display_errors', 1); 
-ini_set('display_startup_errors', 1); 
-error_reporting(E_ALL);
-include __DIR__.'/vendor/autoload.php';
-use RestCord\DiscordClient;
+<?php $url = 'https://discordapp.com/api/users/334048043638849536';
 
-$token = file_get_contents("/home/pi/palantirToken.txt");
-
-$discord = new DiscordClient(['token' => $token]); 
-
-var_dump($discord->guild->getGuild(['guild.id' => 715874397025468417]));
-?>
+$ch = curl_init();
+$token = file_get_contents("/home/pi/palantirToken.txt")
+curl_setopt_array($ch, array(
+    CURLOPT_URL            => $url, 
+    CURLOPT_HTTPHEADER     => array('Authorization: Bot ' . $token)
+));
+$response = curl_exec($ch);
+curl_close($ch); 
+var_dump($response);?>

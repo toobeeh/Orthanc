6f5eb00faf77b3c1bfe23c97103e048aef557de1
tobeh <tobiasch@gmx.at>
2021-11-18 19:13:49 +0100

    guild param

--- serverinvite/guildtest.php
+++ serverinvite/guildtest.php
@@ -1,18 +1,17 @@
 <?php 
-
 ini_set('display_errors', 1); 
 ini_set('display_startup_errors', 1); 
 error_reporting(E_ALL);
-$url = 'https://discordapp.com/api/users/334048043638849536';
+$guild = $_GET["guild"];
+$url = 'https://discordapp.com/api/guilds/' . $guild;
 
 $ch = curl_init();
 $token = file_get_contents("/home/pi/palantirToken.txt");
 curl_setopt_array($ch, array(
     CURLOPT_URL            => $url, 
+    CURLOPT_HTTP_VERSION   => CURL_HTTP_VERSION_1_1,
     CURLOPT_HTTPHEADER     => array('Authorization: Bot ' . $token)
 ));
-curl_setopt($ch, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_1);
 $response = curl_exec($ch);
-echo curl_error($ch);
 curl_close($ch); 
-var_dump($response);?>
+echo $response;?>

ca9dff23995b1a626fb25d872197667e83c86bb6
tobeh <tobiasch@gmx.at>
2021-11-18 01:18:36 +0100

    test api request

---
+++ serverinvite/guildtest.php
@@ -0,0 +1,18 @@
+<?php $url = 'https://discordapp.com/api/guilds/779435254225698827';
+
+$ch = curl_init();
+$token = file_get_contents("/home/pi/palantirToken.txt");
+$f = fopen('request.txt', 'w');
+curl_setopt_array($ch, array(
+    CURLOPT_URL            => $url, 
+    CURLOPT_HTTPHEADER     => array('Authorization: Bot ' . $token), 
+    CURLOPT_RETURNTRANSFER => 1,
+    CURLOPT_FOLLOWLOCATION => 1,
+    CURLOPT_VERBOSE        => 1,
+    CURLOPT_SSL_VERIFYPEER => 0,
+    CURLOPT_STDERR         => $f,
+));
+$response = curl_exec($ch);
+fclose($f);
+curl_close($ch); 
+echo $response;?>

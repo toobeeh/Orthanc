a4c32a0e9d458198415fecacc72e754f7d1e56fe
tobeh <tobiasch@gmx.at>
2021-11-18 01:23:05 +0100

    new api endpoint

--- serverinvite/guildtest.php
+++ serverinvite/guildtest.php
@@ -1,19 +1,16 @@
-<?php $url = 'https://discordapp.com/api/guilds/779435254225698827/members';
+<?php $url = 'https://discordapp.com/api/v6/guilds/779435254225698827/members';
 
 $ch = curl_init();
 $token = file_get_contents("/home/pi/palantirToken.txt");
-echo $token;
-$f = fopen('request.txt', 'w');
+//echo $token;
 curl_setopt_array($ch, array(
     CURLOPT_URL            => $url, 
     CURLOPT_HTTPHEADER     => array('Authorization: Bot ' . $token), 
     CURLOPT_RETURNTRANSFER => 1,
     CURLOPT_FOLLOWLOCATION => 1,
     CURLOPT_VERBOSE        => 1,
-    CURLOPT_SSL_VERIFYPEER => 0,
-    CURLOPT_STDERR         => $f,
+    CURLOPT_SSL_VERIFYPEER => 0
 ));
 $response = curl_exec($ch);
-fclose($f);
 curl_close($ch); 
 echo $response;?>

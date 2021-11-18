4ba46cf715001abd51cc355d21eadac01e98d920
tobeh <tobiasch@gmx.at>
2021-11-18 18:49:06 +0100

    test restcord

--- serverinvite/guildtest.php
+++ serverinvite/guildtest.php
@@ -1,18 +1,13 @@
-<?php $url = $_GET["api"];
+<?php $guild = $_GET["guild"];
 ini_set('display_errors', 1); 
 ini_set('display_startup_errors', 1); 
 error_reporting(E_ALL);
-$ch = curl_init();
+include __DIR__.'/vendor/autoload.php';
+use RestCord\DiscordClient;
+
 $token = file_get_contents("/home/pi/palantirToken.txt");
-//echo $token;
-curl_setopt_array($ch, array(
-    CURLOPT_URL            => $url, 
-    CURLOPT_HTTPHEADER     => array('Authorization: Bot ' . $token), 
-    CURLOPT_RETURNTRANSFER => 1,
-    CURLOPT_FOLLOWLOCATION => 1,
-    CURLOPT_VERBOSE        => 1,
-    CURLOPT_SSL_VERIFYPEER => 0
-));
-$response = curl_exec($ch);
-curl_close($ch); 
-echo $response;?>
+
+$discord = new DiscordClient(['token' => $token]); 
+
+var_dump($discord->guild->getGuild(['guild.id' => 715874397025468417]));
+?>

4a199b620ac2e34152a96cfd5a9c8280375334ee
tobeh <tobiasch@gmx.at>
2021-11-18 01:07:49 +0100

    server card

--- serverinvite/index.php
+++ serverinvite/index.php
@@ -3,6 +3,8 @@
 ini_set('display_errors', 1); 
 ini_set('display_startup_errors', 1); 
 error_reporting(E_ALL);
+include '/home/pi/Webroot/Orthanc/db.php';
+$palantir = json_decode(getPalantirJSON($_GET["token"]));
 if(false == strpos($_SERVER['HTTP_USER_AGENT'], "Discordbot")) header("Location: http://typo.rip#u"); 
 ?>
     <!DOCTYPE html>
@@ -10,13 +12,11 @@ if(false == strpos($_SERVER['HTTP_USER_AGENT'], "Discordbot")) header("Location:
 <head>
 <title>Connect Server to Palantir</title>
 <meta charset="UTF-8">
-<!-- Twitter Card data -->
+<meta property="og:title"  content="<?php echo $palantir->GuildName ?> is using Palantir!" />
+<meta property="og:type" content="website" />
+<meta property="og:url" content="https://typo.rip#u" />
+<meta property="og:image" content="http://my.site.com/images/thumb.png" />
+<meta property="og:description" content="Connect your account to <?php echo $palantir->GuildName ?> by clicking the link!" />
+<meta name="theme-color" content="#FF0000">
 <meta name="twitter:card" content="summary_large_image">
-<!-- Open Graph data -->
-<meta property="og:title" content=">Connect Server to Palantir" />
-<meta property="og:url" content="https://" />
-<meta property="og:image" content="https://" />
-<meta property="og:site_name" content="typo.rip" />
-<meta name="description" content="Crash course on Python offered by Google on Coursera and curated by The Wind LLC">
-<meta name="author" content="C Brennan Poole">
 </head>

674a32872f6571202576f5ebc95fa33e379d3f26
tobeh <tobiasch@gmx.at>
2021-11-18 20:14:03 +0100

    card with discord api details

--- serverinvite/guildtest.php
+++ serverinvite/guildtest.php
@@ -3,16 +3,16 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1); 
 error_reporting(E_ALL);
 $guild = $_GET["guild"];
-$url = 'https://discordapp.com/api/guilds/' . $guild;
 
-$ch = curl_init();
+$url = 'https://discordapp.com/api/guilds/' . $guild;
 $token = file_get_contents("/home/pi/palantirToken.txt");
+$ch = curl_init();
 curl_setopt_array($ch, array(
     CURLOPT_URL            => $url, 
     CURLOPT_HTTP_VERSION   => CURL_HTTP_VERSION_1_1,
+    CURLOPT_RETURNTRANSFER => true,
     CURLOPT_HTTPHEADER     => array('Authorization: Bot ' . $token)
 ));
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); 
 $response = curl_exec($ch);
 $apiGuild = json_decode($response);
 curl_close($ch); 
--- serverinvite/index.php
+++ serverinvite/index.php
@@ -4,19 +4,34 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1); 
 error_reporting(E_ALL);
 include '/home/pi/Webroot/Orthanc/db.php';
+// if request is not from discord preview bot, instant redirect
+if(strpos($_SERVER['HTTP_USER_AGENT'], "Discordbot") == false) header("Location: http://typo.rip#u"); 
+// else generate card
+
 $palantir = json_decode(getPalantirJSON($_GET["token"]));
-if(false == strpos($_SERVER['HTTP_USER_AGENT'], "Discordbot")) header("Location: http://typo.rip#u"); 
+$url = 'https://discordapp.com/api/guilds/' . $palantir->GuildID;
+$token = file_get_contents("/home/pi/palantirToken.txt");
+// get guild from api
+$ch = curl_init();
+curl_setopt_array($ch, array(
+    CURLOPT_URL            => $url, 
+    CURLOPT_HTTP_VERSION   => CURL_HTTP_VERSION_1_1,
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_HTTPHEADER     => array('Authorization: Bot ' . $token)
+));
+$response = curl_exec($ch);
+$apiGuild = json_decode($response);
 ?>
-    <!DOCTYPE html>
+<!DOCTYPE html>
 <html>
 <head>
 <title>Connect Server to Palantir</title>
 <meta charset="UTF-8">
-<meta property="og:title"  content="🔮 <?php echo $palantir->GuildName ?> uses Palantir" />
+<meta property="og:title"  content="🔮 <?php echo $apiGuild->name ?> uses Palantir" />
 <meta property="og:type" content="website" />
 <meta property="og:url" content="https://typo.rip#u" />
-<meta property="og:image" content="http://my.site.com/images/thumb.png" />
+<meta property="og:image" content="<?php echo "<img src=\"https://cdn.discordapp.com/icons/" . $apiGuild->id . "/" . $apiGuild->icon . ".png"?>" />
 <meta property="og:description" content="🔗 Connect your account to <?php echo $palantir->GuildName ?> by clicking the link" />
 <meta name="theme-color" content="#FF0000">
-<meta name="twitter:card" content="summary_large_image">
+<!-- <meta name="twitter:card" content="summary_large_image"> -->
 </head>

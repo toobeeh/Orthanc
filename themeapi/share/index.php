1d57edca1abf5b391ef903d4caec2c1a41a207da
tobeh <tobiasch@gmx.at>
2023-04-07 14:20:18 +0200

    use official example

--- themeapi/share/index.php
+++ themeapi/share/index.php
@@ -4,33 +4,22 @@ ini_set('display_errors', 'On');
 
 $api_dev_key = file_get_contents('/home/pi/pastebin_apikey');
 $api_paste_code = $_POST['theme']; // Get the code from the POST body
-$api_paste_private = "0"; // 0 = public, 1 = unlisted, 2 = private
-$api_paste_name = "Typo Theme Share"; // Use a default name if no name is specified
-$api_paste_expire_date = "N"; // N = never, 10M = 10 minutes, 1H = 1 hour, 1D = 1 day, 1W = 1 week, 2W = 2 weeks, 1M = 1 month
-$api_paste_format = "JSON"; // Let Pastebin detect the format automatically
-$api_paste_option = "paste";
+$api_paste_private 		= '1'; // 0=public 1=unlisted 2=private
+$api_paste_name			= 'Typo Theme Post'; // name or title of your paste
+$api_paste_expire_date 		= 'N';
+$api_paste_format 		= 'json';
+$api_paste_name			= urlencode($api_paste_name);
+$api_paste_code			= urlencode($api_paste_code);
 
-$url = "https://pastebin.com/api/api_post.php";
+$url 				= 'https://pastebin.com/api/api_post.php';
+$ch 				= curl_init($url);
 
-$data = [
-    'api_dev_key' => $api_dev_key,
-    'api_paste_code' => $api_paste_code,
-    'api_paste_option' => $api_paste_option
-];
+curl_setopt($ch, CURLOPT_POST, true);
+curl_setopt($ch, CURLOPT_POSTFIELDS, 'api_option=paste&api_user_key='.$api_user_key.'&api_paste_private='.$api_paste_private.'&api_paste_name='.$api_paste_name.'&api_paste_expire_date='.$api_paste_expire_date.'&api_paste_format='.$api_paste_format.'&api_dev_key='.$api_dev_key.'&api_paste_code='.$api_paste_code.'');
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_VERBOSE, 1);
+curl_setopt($ch, CURLOPT_NOBODY, 0);
 
-echo $data;
-
-$options = [
-    'http' => [
-        'header' => "Content-type: application/x-www-form-urlencoded\r\n",
-        'method' => 'POST',
-        'content' => http_build_query($data)
-    ]
-];
-
-$context = stream_context_create($options);
-$result = file_get_contents($url, false, $context);
-
-echo "kek" . $result; // This will output the URL of the newly created paste
-
+$response  			= curl_exec($ch);
+echo $response;
 ?>

bc03167764a6ff5f0dfdafc38bae03c9a37b85d7
tobeh <tobiasch@gmx.at>
2023-04-07 13:45:03 +0200

    idk as well

--- themeapi/share/index.php
+++ themeapi/share/index.php
@@ -7,7 +7,7 @@ $api_paste_code = $_POST['theme']; // Get the code from the POST body
 $api_paste_private = "0"; // 0 = public, 1 = unlisted, 2 = private
 $api_paste_name = "Typo Theme Share"; // Use a default name if no name is specified
 $api_paste_expire_date = "N"; // N = never, 10M = 10 minutes, 1H = 1 hour, 1D = 1 day, 1W = 1 week, 2W = 2 weeks, 1M = 1 month
-$api_paste_format = ""; // Let Pastebin detect the format automatically
+$api_paste_format = "JSON"; // Let Pastebin detect the format automatically
 
 $url = "https://pastebin.com/api/api_post.php";
 
@@ -19,6 +19,8 @@ $data = [
     'api_paste_expire_date' => $api_paste_expire_date,
     'api_paste_format' => $api_paste_format
 ];
+
+echo $data;
 
 $options = [
     'http' => [
